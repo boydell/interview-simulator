4b0ec33547065104c1cf8da840c19452ed5ee167
Boydell Bown <38111+boydell@users.noreply.github.com>
2023-03-17 16:08:20 -0600

    update the requirements, add the ability to set industry and job title

--- interview.py
+++ interview.py
@@ -2,14 +2,24 @@ import os
 import openai
 import streamlit as st
 from streamlit_chat import message
+from dotenv import load_dotenv
 
-openai.api_key = os.environ.get('openai-key')
-st.session_state['industry'] = 'Healthcare'
-st.session_state['job title'] = 'Software Engineer'
+load_dotenv()
+openai.api_key = os.getenv('openai-key')
+if 'started' not in st.session_state:
+  st.session_state['started'] = False
+if 'generated' not in st.session_state:
+  st.session_state['generated'] = []
+
+# Stuff to add:
+# * linkedIn links, twitter, etc?
+# * Instructions. Include advice for how to respond.
+# * industry, and jog title.
+# * Maybe include in the prompt, if it doesn't get enough detail, ask for more.
 
 def reset():
   type = ("You are a hiring manager for a " + st.session_state['industry'] + 
-    " company, hiring a " + st.session_state['job title'] + "." +
+    " company, hiring a " + st.session_state['job-title'] + "." +
     " Ask the candidate 6 questions, one at a time, and then ask the candidate if they have" +
     " any questions for you. Make all the responses as concise as possible with a little humor expression.")
   st.session_state['prompts'] = [{"role": "system", "content": type}]
@@ -28,8 +38,14 @@ def generate_response(prompt):
   message=completion.choices[0].message.content
   return message
 
-def end_click():
+def start_click():
+  st.session_state['started'] = True
   reset()
+
+def end_click():
+  st.session_state['past'] = []
+  st.session_state['generated'] = []
+  st.session_state['started'] = False
 
 def chat_click():
   if st.session_state['user'] != '':
@@ -41,16 +57,23 @@ def chat_click():
     st.session_state['prompts'].append({"role": "assistant", "content": output})
     st.session_state['user'] = ""
 
-if 'prompts' not in st.session_state:
-  reset()
+# ---- Now do the things
 
-# Now do the things
 # st.image("{Your logo}", width=80)
 st.title("Interview Simulator")
-end_button=st.button("Start Over", on_click=end_click)
+st.markdown("""This is a tool to practice interviews. Every time you run it, the questions will be similar, 
+  but different. It will ask five questions, then ask if you have any questions. Think seriously 
+  how you would answer the questions in a real interview, and the simulator will give you a little
+  bit of feedback for each answer.""")
 
-user_input=st.text_area("You:", key="user")
-chat_button=st.button("Send", on_click=chat_click)
+if st.session_state["started"] == False:
+  industry = st.text_input("Industry", key="industry")
+  job_title = st.text_input("Job Title", key="job-title")
+  start_button = st.button("Start", on_click=start_click)
+else:
+  end_button=st.button("Start Over", on_click=end_click)
+  user_input=st.text_area("You:", key="user")
+  chat_button=st.button("Send", on_click=chat_click)
 
 if st.session_state['generated']:
   for i in range(len(st.session_state['generated'])-1, -1, -1):
